6a9e9e2d4269cce3045f53eb97397f43cf2b43fa
Kazar <kardos@kros.sk>
2019-05-31 12:31:30 +0200

    po obede

---
+++ EntityFrameworkTest/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,56 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class BaseRepository<TEntity, TKey> : IRepository<TEntity, TKey>
+        where TEntity : class, IEntity<TKey>
+    {
+        protected DbSet<TEntity> DbSet { get; }
+        public BaseRepository(DbSet<TEntity> dbSet)
+        {
+            DbSet = dbSet;
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EntityFrameworkTest/Data/Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public interface IRepository<TEntity, TKey>
+        where TEntity:class, IEntity<TKey>
+    {
+        List<TEntity> GetAll();
+        List<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate);
+
+        TEntity GetById(TKey id);
+        TEntity Add(TEntity entity);
+        TEntity Update(TEntity entity);
+        void Delete(TEntity entity);
+        void Delete(TKey id);
+    }
+}
